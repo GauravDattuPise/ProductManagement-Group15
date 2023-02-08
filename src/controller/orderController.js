1f125a1d5dd9715d6e7ef2584af7e7089f16471b
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-02-08 15:17:12 +0530

    almost-done/2

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -20,9 +20,12 @@ const createOrder = async function(req,res){
         let data = req.body
         let userId = req.params.userId
         if(Object.keys(data).length===0) return res.status(400).send({status:false,message:"pls send cartId"})
-        let{cartId,...rest} = data
+        let{cartId, cancellable,...rest} = data
         if(Object.keys(rest).length>0) return res.status(400).send({status:false,message:"pls send valid fields"})
-      
+        if(cancellable){
+
+            if(!["true","false"].includes(cancellable)) return res.status(400).send({status:false,message:"enter 'false' , if you want your order not cancellable"})
+        }
         let cartCheck = await cartModels.findOne({_id:cartId,userId:userId}).select({__v:0,createdAt:0,updatedAt:0})
         if(!cartCheck) return res.status(404).send({status:false,message:"Cart Not found"})
          let count = 0
@@ -39,7 +42,7 @@ const createOrder = async function(req,res){
         }
 
         let createOrder = await orderModel.create(orderData)
-
+        await cartModels.findOneAndUpdate({_id:cartId},{$set:{items:[],totalItems:0,totalPrice:0}},{new:true})
         return res.status(200).send({status:true,message:"Success",data:createOrder})
 
     } catch (error) {
@@ -55,18 +58,19 @@ const updateOrder = async(req,res)=>{
     let {orderId,status,...rest} = data
 
     if(!orderId) return res.status(400).send({status:false,message:"provide orderId for update order"})
-    if(!status) return res.status(400).send({status:false,message:"provide status for update order, [ completed cancled]"})
+    if(!status) return res.status(400).send({status:false,message:"status is required , provide status for update order, [ completed or cancelled]"})
     let checkOrder = await orderModel.findOne({_id:orderId,userId:userId})
     if(!checkOrder) return res.status(404).send({status:false,message:"Order not found"})
   
-    if(status == "pending" ) return res.status(400).send({status:false,message:"order status is default pending, you cannot set maually to 'pending', Use [ completed, cancled] "})
-    if(!["completed", "cancled"].includes(status)) return res.status(400).send({status:false,message:"send valid status, [ completed, cancled]"})
+    if(status == "pending" ) return res.status(400).send({status:false,message:"order status is default pending, you cannot set maually to 'pending', Use [ completed, cancelled] "})
+    if(!["completed", "cancelled"].includes(status)) return res.status(400).send({status:false,message:"send valid status, [ completed, cancelled]"})
     let checkCancel = checkOrder.cancellable
     let updateDoc = {
 
     }
-    if(checkOrder.status == "cancled" ) return res.status(400).send({status:false,message:"Given order already cancelled"})
-    if(checkOrder.status == "completed" ) return res.status(400).send({status:false,message:"Given order has been completed, You cannot cancel this order"})
+
+    if(checkOrder.status == "cancelled" ) return res.status(400).send({status:false,message:"Given order already cancelled"})
+    if(checkOrder.status == "completed" ) return res.status(400).send({status:false,message:"Given order has been completed, You cannot update this order"})
    
     if(checkCancel == true){
         if(checkOrder.status == "pending") {
